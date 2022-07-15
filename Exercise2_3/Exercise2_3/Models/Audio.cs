c6324db8edc242bd8f4f5441237a1cf12040830a
liliana2327 <23hernandezlili23@gmail.com>
2022-07-15 16:11:59 -0600

    --Avance--

--- Exercise2_3/Exercise2_3/Models/Audio.cs
+++ Exercise2_3/Exercise2_3/Models/Audio.cs
@@ -7,6 +7,12 @@ namespace Exercise2_3.Models
 {
     public class Audio
     {
-        
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public string path { get; set; }
+        [MaxLength(100)]
+        public string descripcion { get; set; }
+  
     }
 }
