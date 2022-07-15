3356f4c3f4d44b134f1c5b5dd20ec188c4037f88
lissien <lissym906@gmail.com>
2022-07-15 16:54:20 -0600

    Audi

--- Exercise2_3/Exercise2_3/Views/ListAudios.xaml.cs
+++ Exercise2_3/Exercise2_3/Views/ListAudios.xaml.cs
@@ -14,6 +14,26 @@ namespace Exercise2_3.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListAudios : ContentPage
     {
-        
+        public ListAudios()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            ListaAudio.ItemsSource = await App.BaseDatos.GetListAudios();//LLENAR LA LISTAA DE COLLECTION
+        }
+        private async void SwipeItem_Invoked(object sender, EventArgs e)
+        {
+            SwipeItem item = sender as SwipeItem;//OBTENER EL ITEM DE SWIPE
+            Audio item2 = (Audio)item.BindingContext; //CONVERTIR SU BINDING A AUDIO
+            await CrossMediaManager.Current.Play(item2.path); //USAR ESE AUDIO PARA MANDAR EL PATH A REPRODUCIR
+        }
+        protected async override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            await CrossMediaManager.Current.Stop();
+        }
+
     }
 }
