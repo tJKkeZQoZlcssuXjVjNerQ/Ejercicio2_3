ec09ddd05dd3350041f8de2f2f33aef791b24638
AlexanderJoker <alexandertinoco12349@gmail.com>
2022-07-15 17:45:56 -0600

    Se añadio Los datos del DataBase

--- Exercise2_3/Exercise2_3/Controllers/DataBase.cs
+++ Exercise2_3/Exercise2_3/Controllers/DataBase.cs
@@ -9,6 +9,32 @@ namespace Exercise2_3.Controllers
 {
     public class DataBase
     {
-        
+        readonly SQLiteAsyncConnection db;
+        public DataBase(String path)
+        {
+            db = new SQLiteAsyncConnection(path);
+            db.CreateTableAsync<Audio>().Wait();
+        }
+        #region DATOS DE AUDIO
+        public Task<List<Audio>> GetListAudios()
+        {
+            return db.Table<Audio>().ToListAsync();
+        }
+        public Task<Audio> GetAudiosporId(int id)
+        {
+            return db.Table<Audio>()
+                .Where(i => i.id == id)
+                .FirstOrDefaultAsync();
+        }
+        public Task<int> guardaAudios(Audio mp3)
+        {
+            return mp3.id != 0 ? db.UpdateAsync(mp3) : db.InsertAsync(mp3);
+        }
+        public Task<int> borrarAudios(Audio mp3)
+        {
+            return db.DeleteAsync(mp3);
+        }
+        #endregion
+
     }
 }
